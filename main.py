26b37a55ba84837c4d11093e0e104d5e03cd718f
joel-rocha <joelrochachaves@gmail.com>
2025-08-15 15:05:12 -0300

    fiz a separação parcial do menu e comecei a trabalhar com data  e hora

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
+import ntplib
+from datetime import datetime, timezone, timedelta
 
-def processar_imagem():
-    print("dgfgc")
+def pegar_data_hora_ntp():
+    try:
+        cliente = ntplib.NTPClient()
+        resposta = cliente.request('pool.ntp.org')
+        data_hora = datetime.fromtimestamp(resposta.tx_time, timezone.utc)
+        return data_hora
+    except Exception as e:
+        print("Erro:", e)
+        return None
+
+def carregarCardapio():
+    refeicoes = []
+    with open("cardapio.csv", "r", newline="", encoding="utf-8") as arquivoCardapio:
+        for linha in arquivoCardapio:
+            linha = linha.strip()
+
+            if not linha:  # pula linhas vazias
+                continue
+
+            partesRefeicao = linha.split(",", 2)
+
+            if len(partesRefeicao) == 3:
+                data = partesRefeicao[0].strip()
+                periodo = partesRefeicao[1].strip()
+                itens = partesRefeicao[2].strip().replace(";", "\n")
+                refeicoes.append([data, periodo, itens])
+
+    return refeicoes
 
 
 if __name__ == '__main__':
-    processar_imagem()
+    dados = carregarCardapio()
+    agora = pegar_data_hora_ntp()
+    brasil = agora - timedelta(hours=3)
+
+    data = brasil.date()
+    hora = brasil.time()
+
+    for data, periodo, itens in dados:
+        print(f"{periodo}")
+        print(f"{itens}")
+        print("-" * 40)
